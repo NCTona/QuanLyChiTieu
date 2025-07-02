0bde2f4455f74ccf823f7b431a2356065d4a4a0d
NCTona <mnbtnbnk1906@gmail.com>
2025-07-02 15:04:59 +0700

    final_commit

--- app/src/main/java/com/example/jetpackcompose/app/AppQuanLyChiTieu.kt
+++ app/src/main/java/com/example/jetpackcompose/app/AppQuanLyChiTieu.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -158,6 +159,7 @@ fun AppQuanLyChiTieu(transactionStorage: TransactionStorage) {
                 val email = it.arguments?.getString("email") ?: ""
                 OTPContent(navController, email)
             }
+
             composable("mainscreen") { MainScreen(navController) }
             composable("anual") { AnualScreen(navController) }
             composable("other") { OtherScreen(navController) }
